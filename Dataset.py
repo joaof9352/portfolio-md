7ef1c3afa5a6ef19ef83983f77f244d19af05886
João Ferreira <joaof9352@gmail.com>
2023-03-05 01:59:15 +0000

    Update Dataset.py

--- Dataset.py
+++ Dataset.py
@@ -2,30 +2,75 @@ import numpy as np
 
 class Dataset:
     def __init__(self):
+        """
+            TODO: Deveríamos guardar os "encodings"?
+        """
         self.X = None
         self.y = None
         self.feature_names = None
         self.label_name = None
+        self.nums = None
+        self.cats = None
 
-    def load_from_csv(self, filename, label_name=None):
-        with open(filename, 'r') as f:
-            lines = f.readlines()
-        header = lines[0].strip().split(',')
-        data = [line.strip().split(',') for line in lines[1:]]
+    def __read_datatypes(self, filename, sep):
+        """
+            Lê os tipos da primeira linha do arquivo e armazena em self.nums ou self.cats o numero da coluna.
+
+            TODO: Se a primeira linha tiver um missing value é assumida como categórica ...
+        """
+        with open(filename) as f:
+            header = f.readline().rstrip().split(sep)
+
+            self.feature_names = header[:-1]
+            self.label_name = header[-1]
+            line = f.readline().rstrip().split(sep)
+
+            self.nums = []
+            self.cats = []
+
+            for i in range(len(line)):
+                try:
+                    float(line[i])
+                    self.nums.append(i)
+                except ValueError:
+                    self.cats.append(i)
+
+
+    def load(self, filename, sep = ',', label_name=None):
+        """
+            Carrega o dataset do arquivo filename, separado por sep.
+            Feature_names, label_name, X e y são armazenados como atributos da classe.
+
+            TODO: Quando fazemos encoding de dados categóricos, os missing values são considerados como uma categoria.
+        """
+        self.__read_datatypes(filename, sep)
+
+        numeric_data = np.genfromtxt(filename, delimiter=sep, usecols=self.nums, skip_header=True)
+        categorical_data = np.genfromtxt(filename, dtype='U', delimiter=sep, usecols=self.cats, skip_header=True)
+        
+        # encode categorical_data
+        categorical_data = self.label_encode(categorical_data)
+
+        data = np.empty_like(np.concatenate((numeric_data, categorical_data), axis=1))
+        data[:, self.nums] = numeric_data
+        data[:, self.cats] = categorical_data
+
         self.X = np.array(data)[:, :-1]
         self.y = np.array(data)[:, -1]
-        self.feature_names = header[:-1]
-        self.label_name = header[-1] if label_name is None else label_name
 
-    def load_from_tsv(self, filename, label_name=None):
-        with open(filename, 'r') as f:
-            lines = f.readlines()
-        header = lines[0].strip().split('\t')
-        data = [line.strip().split('\t') for line in lines[1:]]
-        self.X = np.array(data)[:, :-1]
-        self.y = np.array(data)[:, -1]
-        self.feature_names = header[:-1]
-        self.label_name = header[-1] if label_name is None else label_name
+        if label_name:
+            self.label_name = label_name
+
+    def label_encode(self, arr):
+        encoded_arr = np.empty_like(arr)
+
+        if len(arr.shape) == 1:
+            unique_vals, encoded_arr[:] = np.unique(arr[:], return_inverse=True)
+        else: 
+            for i in range(arr.shape[1]):
+                unique_vals, encoded_arr[:,i] = np.unique(arr[:,i], return_inverse=True)
+        
+        return encoded_arr
 
     def describe(self):
         print('Dataset summary:')
@@ -75,6 +120,6 @@ class Dataset:
 
 
 d = Dataset()
-d.load_from_csv('teste.csv')
-print(d.X)
-print(d.count_missing_values())
+d.load('teste.csv')
+print(d.describe())
+#print(d.count_missing_values())
