6f578b182741d02d19a46fde8b1debdd8981bde8
shaggy <jocaldas11@gmail.com>
2023-03-09 22:54:05 +0000

    f_classif começado

--- f_classif.py
+++ f_classif.py
@@ -1,3 +1,13 @@
 import numpy as np
-
+import scipy as stats
+from typing import Tuple, Union
 import Dataset
+
+def f_classif(dataset: Dataset) -> Union[Tuple[np.ndarray,np.ndarray],Tuple[float,float]]:
+
+    classes = 0 #temos de sacar as classes do dataset
+    grupos = 0 #os grupos por classe que presumo que seja a unica existente noo dataset.py
+
+    F, p = stats.f_oneway(grupos)
+
+    return F, p
