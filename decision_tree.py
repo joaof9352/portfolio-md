Fix variance

decision_tree.py
@@ -1,160 +1,40 @@
 import numpy as np
 from Dataset import Dataset
 
-class DecisionTree:
-    def __init__(self, attribute_selection='entropy', pruning=None):
-        self.attribute_selection = attribute_selection
-        self.pruning = pruning
-        self.tree = None
+class Decision_tree:
+
+    def __init__(self):
+        pass
 
     def fit(self, X, y):
-        attributes = np.arange(X.shape[1])  # indices das colunas de atributos
-        self.tree = self._build_tree(X, y, attributes)
+        pass
 
     def predict(self, X):
-        predictions = np.zeros(X.shape[0])
-        for i, instance in enumerate(X):
-            predictions[i] = self._traverse_tree(instance, self.tree)
-        return predictions
+        pass
 
-    def __repr__(self):
-        return self._print_tree(self.tree)
+    def _entropy(self, X, y):
+        for i in range(X.shape[1]):
+            number_occurences = {}
+            for j in range(X.shape[0]):
+                if X[j, i] in number_occurences:
+                    number_occurences[X[j, i]] += 1
+                else:
+                    number_occurences[X[j, i]] = 1
+        
+        total = number_occurences.items().apply(lambda x: x[1]).sum()
+        probabilities = [v/total for _, v in number_occurences.items()]
+        entropy = 0
+        for probability in probabilities:
+            entropy += probability * np.log2(probability)
 
-    def _build_tree(self, X, y, attributes):
-        if len(np.unique(y)) == 1:  # todos os exemplos possuem a mesma classe
-            return {'class': y[0]}
+        return -entropy
 
-        if len(attributes) == 0:  # não há mais atributos para dividir
-            unique_classes, class_counts = np.unique(y, return_counts=True)
-            majority_class = unique_classes[np.argmax(class_counts)]
-            return {'class': majority_class}
 
-        # Seleção do atributo de acordo com o critério escolhido
-        if self.attribute_selection == 'entropy':
-            selected_attribute = self._select_attribute_entropy(X, y, attributes)
-        elif self.attribute_selection == 'gini':
-            selected_attribute = self._select_attribute_gini(X, y, attributes)
-        elif self.attribute_selection == 'gain_ratio':
-            selected_attribute = self._select_attribute_gain_ratio(X, y, attributes)
-        else:
-            raise ValueError("Invalid attribute selection method.")
-
-        tree = {'attribute': selected_attribute, 'children': {}}
-
-        # Construção dos filhos
-        attribute_values = np.unique(X[:, selected_attribute])
-        for value in attribute_values:
-            subset_mask = X[:, selected_attribute] == value
-            subset_X = X[subset_mask]
-            subset_y = y[subset_mask]
-            if subset_X.shape[0] == 0:  # nenhum exemplo com esse valor do atributo
-                unique_classes, class_counts = np.unique(y, return_counts=True)
-                majority_class = unique_classes[np.argmax(class_counts)]
-                tree['children'][value] = {'class': majority_class}
-            else:
-                remaining_attributes = np.delete(attributes, selected_attribute)
-                tree['children'][value] = self._build_tree(subset_X, subset_y, remaining_attributes)
-
-        return tree
-
-    def _select_attribute_entropy(self, X, y, attributes):
-        entropy = self._calculate_entropy(y)
-        information_gains = np.zeros(len(attributes))
-        for i, attribute in enumerate(attributes):
-            attribute_values = np.unique(X[:, attribute])
-            subset_entropies = np.zeros(len(attribute_values))
-            for j, value in enumerate(attribute_values):
-                subset_mask = X[:, attribute] == value
-                subset_y = y[subset_mask]
-                subset_entropies[j] = self._calculate_entropy(subset_y)
-            subset_sizes = np.array([len(X[X[:, attribute] == value]) for value in attribute_values])
-            information_gains[i] = entropy - np.sum(subset_entropies * subset_sizes) / len(y)
-        return attributes[np.argmax(information_gains)]
-
-    def _calculate_entropy(self, y):
-        _, class_counts = np.unique(y, return_counts=True)
-        probabilities = class_counts / len(y)
-        return -np.sum(probabilities * np.log2(probabilities))
-
-    def _select_attribute_gini(self, X, y, attributes):
-        gini_index = self._calculate_gini_index(y)
-        gini_indices = np.zeros(len(attributes))
-        for i, attribute in enumerate(attributes):
-            attribute_values = np.unique(X[:, attribute])
-            subset_ginis = np.zeros(len(attribute_values))
-            for j, value in enumerate(attribute_values):
-                subset_mask = X[:, attribute] == value
-                subset_y = y[subset_mask]
-                subset_ginis[j] = self._calculate_gini_index(subset_y)
-            subset_sizes = np.array([len(X[X[:, attribute] == value]) for value in attribute_values])
-            gini_indices[i] = np.sum(subset_ginis * subset_sizes) / len(y)
-        return attributes[np.argmin(gini_indices)]
-
-    def _calculate_gini_index(self, y):
-        _, class_counts = np.unique(y, return_counts=True)
-        probabilities = class_counts / len(y)
-        return 1 - np.sum(probabilities ** 2)
-
-    def _select_attribute_gain_ratio(self, X, y, attributes):
-        selected_attribute = self._select_attribute_entropy(X, y, attributes)
-        attribute_values = np.unique(X[:, selected_attribute])
-        subset_ratios = np.zeros(len(attribute_values))
-        for i, value in enumerate(attribute_values):
-            subset_mask = X[:, selected_attribute] == value
-            subset_y = y[subset_mask]
-            subset_ratios[i] = len(subset_y) / len(y)
-        gain_ratios = np.zeros(len(attributes))
-        for i, attribute in enumerate(attributes):
-            if attribute == selected_attribute:
-                continue
-            attribute_values = np.unique(X[:, attribute])
-            subset_entropies = np.zeros(len(attribute_values))
-            for j, value in enumerate(attribute_values):
-                subset_mask = X[:, attribute] == value
-                subset_y = y[subset_mask]
-                subset_entropies[j] = self._calculate_entropy(subset_y)
-            subset_sizes = np.array([len(X[X[:, attribute] == value]) for value in attribute_values])
-            gain_ratios[i] = self._calculate_entropy(y) - np.sum(subset_entropies * subset_sizes) / len(y)
-        return attributes[np.argmax(gain_ratios * subset_ratios)]
-
-    def _traverse_tree(self, instance, node):
-        if 'class' in node:
-            return node['class']
-        attribute = node['attribute']
-        value = instance[attribute]
-        if value not in node['children']:
-            return None  # valor do atributo não presente na árvore (tratamento de conflito)
-        return self._traverse_tree(instance, node['children'][value])
-
-    def _print_tree(self, node, indent=''):
-        if 'class' in node:
-            return str(node['class'])
-        attribute = node['attribute']
-        tree_str = ''
-        for value, child in node['children'].items():
-            tree_str += f'{indent}{attribute} = {value} -> {self._print_tree(child, indent + "  ")}\n'
-        return tree_str
+    def _choose_sub_classes(self):
+        pass
 
 d = Dataset()
 d.load(filename='notas.csv')
-#print(_entropy(d.X, d.y))
 
-from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(d.X, d.y, test_size=0.2, random_state=42)
-
-tree = DecisionTree(attribute_selection='entropy')
-
-# Treinando a árvore de decisão
-tree.fit(X_train, y_train)
-
-# Imprimindo a árvore
-print(tree)
-
-from sklearn.metrics import accuracy_score
-
-# Fazendo previsões no conjunto de teste
-y_pred = tree.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print(f'Acurácia: {accuracy}')
 

naivebayes.py
@@ -10,70 +10,45 @@ class NaiveBayes:
         self.class_likelihoods = None
 
     def fit(self, X, y):
-        # Obtém as classes únicas presentes nos rótulos
         self.classes = np.unique(y)
-        # Inicializa os arrays de probabilidades a priori e de verossimilhança de classe
         self.class_priors = np.zeros(len(self.classes))
         self.class_likelihoods = []
 
-        # Para cada classe, calcula a probabilidade a priori e a verossimilhança
         for i, c in enumerate(self.classes):
-            # Filtra as amostras que pertencem à classe atual
             X_c = X[y == c]
-            # Calcula a probabilidade a priori da classe
             self.class_priors[i] = X_c.shape[0] / X.shape[0]
 
-            # Calcula a verossimilhança para cada atributo
             likelihoods = []
             for j in range(X.shape[1]):
-                # Obtém os valores únicos do atributo
                 feature_values = np.unique(X[:, j])
-                # Inicializa o array de verossimilhança para o atributo atual
                 likelihood = np.zeros(len(feature_values))
-                # Calcula a frequência relativa de cada valor do atributo na classe atual
                 for k, v in enumerate(feature_values):
                     likelihood[k] = np.sum(X_c[:, j] == v) / X_c.shape[0]
                 likelihoods.append(likelihood)
-            # Adiciona as verossimilhanças da classe atual à lista de verossimilhanças de classe
             self.class_likelihoods.append(likelihoods)
 
 
     def predict(self, X):
         predictions = []
-        # Para cada amostra de teste
         for sample in X:
             posterior_probs = []
-            # Para cada classe
             for i, c in enumerate(self.classes):
-                # Calcula o logaritmo da probabilidade a priori da classe
                 class_prior = np.log(self.class_priors[i])
-                # Obtém as verossimilhanças da classe atual
                 class_likelihoods = self.class_likelihoods[i]
                 posterior_prob = class_prior
-                # Para cada atributo da amostra
                 for j in range(len(sample)):
                     feature_value = sample[j]
-                    # Verifica se o valor do atributo está presente nos valores únicos do conjunto de treinamento
                     if feature_value in np.unique(X[:, j]):
-                        # Obtém o índice do valor do atributo na lista de valores únicos
                         feature_index = np.where(np.unique(X[:, j]) == feature_value)[0][0]
-                        # Obtém a verossimilhança do valor do atributo na classe atual
                         likelihood = class_likelihoods[j][feature_index]
-                        # Adiciona o logaritmo da verossimilhança ao logaritmo da probabilidade a posteriori
                         posterior_prob += np.log(likelihood + 1e-9)  # Adiciona um valor pequeno para evitar o log(0)
-                # Adiciona a probabilidade a posteriori à lista de probabilidades a posteriori
                 posterior_probs.append(posterior_prob)
-            # Obtém a classe com a maior probabilidade a posteriori como a predição
             prediction = self.classes[np.argmax(posterior_probs)]
-            # Adiciona a predição à lista de predições
             predictions.append(prediction)
-        # Retorna as predições como um array numpy
         return np.array(predictions)
 
     def calcAcc(self, X_test, y_test):
-        # Calcula as predições para o conjunto de teste
         preds = self.predict(X_test)
-        # Calcula a acurácia comparando as predições com os rótulos verdadeiros
         return accuracy_score(y_test, preds)
     
 d = Dataset()
@@ -81,8 +56,7 @@ d.load(filename='numeros.csv')
 d.dropna()
 n = NaiveBayes()
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(d.X, d.y, test_size=0.2, random_state=2023)
+X_train, X_test, y_train, y_test = d.train_test_split(test_size=0.2, random_state=2023)
 
 n.fit(X_train, y_train)
 print(n.calcAcc(X_test, y_test))    

varianceThreshold.py
@@ -29,7 +29,7 @@ class VarianceThreshold:
         features = np.array(dataset.feature_names)[new_features]
 
         d = Dataset()
-        d.setDataset(x=dataset.X, y=dataset.y, feature_names=list(features), label_name=dataset.label_name)
+        d.setDataset(x=X, y=dataset.y, feature_names=list(features), label_name=dataset.label_name)
 
         return d
 
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     dataset.load(filename='numeros.csv')
 
     # Create an instance of SelectKBest
-    threshold = 1.0
+    threshold = 25.0
     variance_threshold = VarianceThreshold(threshold=threshold)
 
     # Fit and transform the dataset